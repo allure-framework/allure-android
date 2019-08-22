bae816de171611e4852822e9e9c6c535c9241853
Victor Orlovsky <viclovsky@gmail.com>
2019-08-22 17:23:50 +0300

    fix CrashTestActivity, up minSdkVersion

--- allure-android/src/espresso/kotlin/ru/tinkoff/allure/CrashTestActivity.kt
+++ allure-android/src/espresso/kotlin/ru/tinkoff/allure/CrashTestActivity.kt
@@ -1,7 +1,7 @@
 package ru.tinkoff.allure
 
 import android.os.Bundle
-import androidx.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import rx.Observable
 import rx.schedulers.Schedulers
